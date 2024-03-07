460e9a19d8c2da5d1bab11c7b315e6c52ba271f1
roy860535 <roy860535@gmail.com>
2024-03-07 21:27:41 +0800

    Second Commit

--- N09310026_GUI_Script/N09310026_GUI_Script/Form1.Designer.cs
+++ N09310026_GUI_Script/N09310026_GUI_Script/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRabbit = new System.Windows.Forms.Button();
+            this.BtnCrocodile = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(57, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "這裡是動物園";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label2.Location = new System.Drawing.Point(258, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "你可以選擇你要搏鬥的動物";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Cornsilk;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(575, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "你決定??";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnRabbit
+            // 
+            this.btnRabbit.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnRabbit.Location = new System.Drawing.Point(61, 246);
+            this.btnRabbit.Name = "btnRabbit";
+            this.btnRabbit.Size = new System.Drawing.Size(426, 90);
+            this.btnRabbit.TabIndex = 4;
+            this.btnRabbit.Text = "兔子";
+            this.btnRabbit.UseVisualStyleBackColor = true;
+            this.btnRabbit.Click += new System.EventHandler(this.btnrabbit_Click);
+            // 
+            // BtnCrocodile
+            // 
+            this.BtnCrocodile.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BtnCrocodile.Location = new System.Drawing.Point(61, 124);
+            this.BtnCrocodile.Name = "BtnCrocodile";
+            this.BtnCrocodile.Size = new System.Drawing.Size(426, 87);
+            this.BtnCrocodile.TabIndex = 5;
+            this.BtnCrocodile.Text = "鱷魚";
+            this.BtnCrocodile.UseVisualStyleBackColor = true;
+            this.BtnCrocodile.Click += new System.EventHandler(this.BtnCrocodile_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Cornsilk;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(527, 422);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(261, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "多遊四甲  N09310026 魏暐哲";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BtnCrocodile);
+            this.Controls.Add(this.btnRabbit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRabbit;
+        private System.Windows.Forms.Button BtnCrocodile;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- N09310026_GUI_Script/N09310026_GUI_Script/Form1.cs
+++ N09310026_GUI_Script/N09310026_GUI_Script/Form1.cs
@@ -16,5 +16,85 @@ namespace N09310026_GUI_Script
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        int Rabbitfighttime = 0;
+        private void btnrabbit_Click(object sender, EventArgs e)
+        {
+            if (Rabbitfighttime == 0)
+            {
+                btnRabbit.Text = "你與兔子狠狠的纏鬥在了一起";
+            }
+            if(Rabbitfighttime >= 3)
+            {
+                btnRabbit.Text = "你被他的蓬鬆外表給欺騙了，你受了重傷";
+            }
+            if(Rabbitfighttime >= 5)
+            {
+                btnRabbit.Text = "兔子:驕傲吧，你很強";
+            }
+            if(Rabbitfighttime >= 6)
+            {
+                btnRabbit.Text = "你在兔子稱讚的眼光中，慢慢闔上了雙眼";
+                Rabbitfighttime = 0;
+            }
+            Rabbitfighttime++;
+            
+        }
+
+        int Crocodilefighttime = 0;
+
+        private void BtnCrocodile_Click(object sender, EventArgs e)
+        {
+            if (Crocodilefighttime == 0)
+            {
+                BtnCrocodile.Text = "你在與鱷魚的戰鬥中變成了朋友";
+            }
+
+            if (Crocodilefighttime >= 3)
+            {
+                BtnCrocodile.Text = "你有天與鱷魚正在逛街時偶遇了隨機殺人魔兔子";
+            }
+
+            if (Crocodilefighttime >= 5)
+            {
+                BtnCrocodile.Text = "鱷魚為了保護你，留下來殿後";
+            }
+
+            if(Crocodilefighttime >= 6)
+            {
+                BtnCrocodile.Text = "你存活了下來，但是鱷魚的犧牲卻給你帶來了永遠的夢魘，" +
+                    "你為了復仇開始了刻苦的鍛鍊，心中充滿了決心";
+            }
+            if (Crocodilefighttime >= 6)
+            {
+                BtnCrocodile.Text = "日後你與兔子頂上決戰，同歸於盡了";
+                Crocodilefighttime = 0;
+            }
+            Crocodilefighttime++;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
